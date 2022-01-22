d0d5a96751797fe5170d648e269686ff45e2a84c
naman312010 <naman312010@gmail.com>
2022-01-22 11:32:55 +0530

    Initial release

--- generated/schema.ts
+++ generated/schema.ts
@@ -11,31 +11,31 @@ import {
   BigDecimal
 } from "@graphprotocol/graph-ts";
 
-export class ExampleEntity extends Entity {
+export class Token extends Entity {
   constructor(id: string) {
     super();
     this.set("id", Value.fromString(id));
 
-    this.set("count", Value.fromBigInt(BigInt.zero()));
-    this.set("account", Value.fromBytes(Bytes.empty()));
-    this.set("operator", Value.fromBytes(Bytes.empty()));
+    this.set("creator", Value.fromBytes(Bytes.empty()));
+    this.set("type", Value.fromString(""));
+    this.set("totalcopies", Value.fromBigInt(BigInt.zero()));
   }
 
   save(): void {
     let id = this.get("id");
-    assert(id != null, "Cannot save ExampleEntity entity without an ID");
+    assert(id != null, "Cannot save Token entity without an ID");
     if (id) {
       assert(
         id.kind == ValueKind.STRING,
-        "Cannot save ExampleEntity entity with non-string ID. " +
+        "Cannot save Token entity with non-string ID. " +
           'Considering using .toHex() to convert the "id" to a string.'
       );
-      store.set("ExampleEntity", id.toString(), this);
+      store.set("Token", id.toString(), this);
     }
   }
 
-  static load(id: string): ExampleEntity | null {
-    return changetype<ExampleEntity | null>(store.get("ExampleEntity", id));
+  static load(id: string): Token | null {
+    return changetype<Token | null>(store.get("Token", id));
   }
 
   get id(): string {
@@ -47,30 +47,111 @@ export class ExampleEntity extends Entity {
     this.set("id", Value.fromString(value));
   }
 
-  get count(): BigInt {
-    let value = this.get("count");
+  get creator(): Bytes {
+    let value = this.get("creator");
+    return value!.toBytes();
+  }
+
+  set creator(value: Bytes) {
+    this.set("creator", Value.fromBytes(value));
+  }
+
+  get uri(): string | null {
+    let value = this.get("uri");
+    if (!value || value.kind == ValueKind.NULL) {
+      return null;
+    } else {
+      return value.toString();
+    }
+  }
+
+  set uri(value: string | null) {
+    if (!value) {
+      this.unset("uri");
+    } else {
+      this.set("uri", Value.fromString(<string>value));
+    }
+  }
+
+  get type(): string {
+    let value = this.get("type");
+    return value!.toString();
+  }
+
+  set type(value: string) {
+    this.set("type", Value.fromString(value));
+  }
+
+  get totalcopies(): BigInt {
+    let value = this.get("totalcopies");
     return value!.toBigInt();
   }
 
-  set count(value: BigInt) {
-    this.set("count", Value.fromBigInt(value));
+  set totalcopies(value: BigInt) {
+    this.set("totalcopies", Value.fromBigInt(value));
+  }
+}
+
+export class TokenBalance extends Entity {
+  constructor(id: string) {
+    super();
+    this.set("id", Value.fromString(id));
+
+    this.set("owner", Value.fromBytes(Bytes.empty()));
+    this.set("amount", Value.fromBigInt(BigInt.zero()));
+    this.set("token", Value.fromString(""));
   }
 
-  get account(): Bytes {
-    let value = this.get("account");
+  save(): void {
+    let id = this.get("id");
+    assert(id != null, "Cannot save TokenBalance entity without an ID");
+    if (id) {
+      assert(
+        id.kind == ValueKind.STRING,
+        "Cannot save TokenBalance entity with non-string ID. " +
+          'Considering using .toHex() to convert the "id" to a string.'
+      );
+      store.set("TokenBalance", id.toString(), this);
+    }
+  }
+
+  static load(id: string): TokenBalance | null {
+    return changetype<TokenBalance | null>(store.get("TokenBalance", id));
+  }
+
+  get id(): string {
+    let value = this.get("id");
+    return value!.toString();
+  }
+
+  set id(value: string) {
+    this.set("id", Value.fromString(value));
+  }
+
+  get owner(): Bytes {
+    let value = this.get("owner");
     return value!.toBytes();
   }
 
-  set account(value: Bytes) {
-    this.set("account", Value.fromBytes(value));
+  set owner(value: Bytes) {
+    this.set("owner", Value.fromBytes(value));
   }
 
-  get operator(): Bytes {
-    let value = this.get("operator");
-    return value!.toBytes();
+  get amount(): BigInt {
+    let value = this.get("amount");
+    return value!.toBigInt();
   }
 
-  set operator(value: Bytes) {
-    this.set("operator", Value.fromBytes(value));
+  set amount(value: BigInt) {
+    this.set("amount", Value.fromBigInt(value));
+  }
+
+  get token(): string {
+    let value = this.get("token");
+    return value!.toString();
+  }
+
+  set token(value: string) {
+    this.set("token", Value.fromString(value));
   }
 }
--- src/mapping.ts
+++ src/mapping.ts
@@ -11,16 +11,132 @@ import {
 } from "../generated/ERC721/ERC721"
 import { Token, TokenBalance } from "../generated/schema"
 
-export function handleTransferBatch(event: TransferBatch): void {}
+export function handleTransferBatch(event: TransferBatch): void {
+  let erc1155 = ERC1155.bind(event.address);
+  for (let index = 0; index < event.params.ids.length;index++) {
+    let tkid = event.address.toHexString() + "_" + event.params.ids[index].toString()
+    let token = Token.load(tkid)
+    if (token == null) {
+      token = new Token(tkid)
+      token.creator = event.params.to
+      token.type = "ERC1155"
+      token.totalcopies = event.params.values[index]
+      let callUriResult = erc1155.try_uri(event.params.ids[index])
+      if (!callUriResult.reverted)
+        token.uri = callUriResult.value
+      token.save()
 
-export function handleTransferSingle(event: TransferSingle): void {}
+      let balance = new TokenBalance(event.params.to.toHexString() + "_" + tkid)
+      balance.owner = event.params.to
+      balance.token = token.id
+      balance.amount = token.totalcopies
+      balance.save()
+    }
 
-export function handleURI(event: URI): void {}
+    else {
+      let user1 = TokenBalance.load(event.params.from.toHexString() + "_" + tkid)
+      if (user1) {
+        let user2 = TokenBalance.load(event.params.to.toHexString() + "_" + tkid)
+        if (user2 == null) {
+          user2 = new TokenBalance(event.params.to.toHexString() + "_" + tkid)
+          user2.owner = event.params.to
+          user2.token = tkid
+          user2.amount = event.params.values[index]
+        }
+        else
+          user2.amount = user2.amount.plus(event.params.values[index])
+        user1.amount = user2.amount.minus(event.params.values[index])
+        user1.save()
+        user2.save()
+      }
+    }
+  }
+}
 
-export function handleTransfer(event: Transfer): void{
+export function handleTransferSingle(event: TransferSingle): void {
+  let erc1155 = ERC1155.bind(event.address)
+  let tkid = event.address.toHexString() + "_" + event.params.id.toString()
+  let token = Token.load(tkid)
+  if (token == null) {
+    token = new Token(tkid)
+    token.creator = event.params.to
+    token.type = "ERC1155"
+    token.totalcopies = event.params.value
+    let callUriResult = erc1155.try_uri(event.params.id)
+    if (!callUriResult.reverted)
+      token.uri = callUriResult.value
+    token.save()
+
+    let balance = new TokenBalance(event.params.to.toHexString() + "_" + tkid)
+    balance.owner = event.params.to
+    balance.token = token.id
+    balance.amount = token.totalcopies
+    balance.save()
+  }
+
+  else {
+    let user1 = TokenBalance.load(event.params.from.toHexString() + "_" + tkid)
+    if (user1) {
+      let user2 = TokenBalance.load(event.params.to.toHexString() + "_" + tkid)
+      if (user2 == null) {
+        user2 = new TokenBalance(event.params.to.toHexString() + "_" + tkid)
+        user2.owner = event.params.to
+        user2.token = tkid
+        user2.amount = event.params.value
+      }
+      else
+        user2.amount = user2.amount.plus(event.params.value)
+      user1.amount = user2.amount.minus(event.params.value)
+      user1.save()
+      user2.save()
+    }
+  }
+}
+
+export function handleURI(event: URI): void {
+  let token = Token.load(event.address.toHexString() + "_" + event.params.id.toString())
+  if (token) {
+    token.uri = event.params.value
+    token.save()
+  }
+}
+
+export function handleTransfer(event: Transfer): void {
+  let erc721 = ERC721.bind(event.address)
   let tkid = event.address.toHexString() + "_" + event.params.tokenId.toString()
   let token = Token.load(tkid)
-  if(!token){
-    
+  if (token == null) {
+    token = new Token(tkid)
+    token.creator = event.params.to
+    token.type = "ERC721"
+    token.totalcopies = BigInt.fromI32(1)
+    let callUriResult = erc721.try_tokenURI(event.params.tokenId)
+    if (!callUriResult.reverted)
+      token.uri = callUriResult.value
+    token.save()
+
+    let balance = new TokenBalance(event.params.to.toHexString() + "_" + tkid)
+    balance.owner = event.params.to
+    balance.token = token.id
+    balance.amount = token.totalcopies
+    balance.save()
+  }
+
+  else {
+    let user1 = TokenBalance.load(event.params.from.toHexString() + "_" + tkid)
+    if (user1) {
+      let user2 = TokenBalance.load(event.params.to.toHexString() + "_" + tkid)
+      if (user2 == null) {
+        user2 = new TokenBalance(event.params.to.toHexString() + "_" + tkid)
+        user2.owner = event.params.to
+        user2.token = tkid
+        user2.amount = BigInt.fromI32(1)
+      }
+      else
+        user2.amount = BigInt.fromI32(1)
+      user1.amount = BigInt.fromI32(0)
+      user1.save()
+      user2.save()
+    }
   }
 }
